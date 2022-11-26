d5beda427c1ce2599cb16072b146ee6e5c742e39
Igor Amon <48227869+amonigor@users.noreply.github.com>
2022-11-26 15:53:48 -0300

    created popover component

---
+++ components/Popover.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <button class="btn-toggle" :class="{ open }" @click.prevent="toggle">
+      <img :src="icon" />
+    </button>
+
+    <Transition name="bounce" mode>
+      <div v-if="open" class="popover-content">
+        <slot></slot>
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PopoverComponent',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      open: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+
+  .btn-toggle {
+    background-color: $white;
+    border: 1px solid $tertiary-lighter;
+    border-radius: 0.5rem;
+    width: 2.625rem;
+    height: 2.625rem;
+    cursor: pointer;
+    transition-property: background-color;
+    transition-duration: 0.2s;
+
+    &:hover {
+      background-color: $tertiary-hyper-light;
+    }
+
+    &.open {
+      background-color: $primary-light;
+    }
+
+    img {
+      width: 1.5rem;
+      height: 1.5rem;
+      margin-top: 0.2rem;
+    }
+  }
+
+  .popover-content {
+    background-color: $white;
+    position: absolute;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    max-width: 21.875rem;
+  }
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
