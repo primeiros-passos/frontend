88bddcd67a791476907ce27d3977d2644b60519c
Igor Amon <48227869+amonigor@users.noreply.github.com>
2022-12-05 01:21:25 -0300

    created textarea component

---
+++ components/Textarea.vue
@@ -0,0 +1,43 @@
+<template>
+  <textarea
+    rows="5"
+    :placeholder="placeholder"
+    @input="updateValue($event.target.value)"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: 'TextareaComponent',
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    // eslint-disable-next-line vue/require-default-prop
+    value: {
+      type: String,
+    },
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  width: 100%;
+  position: relative;
+  resize: none;
+  padding: 0.625rem 1.875rem;
+  font-size: 1rem;
+  font-family: 'Poppins', sans-serif;
+  border-radius: 1.25rem;
+  border: 1px solid $tertiary-light;
+  background-color: $white;
+  outline-color: $secondary;
+}
+</style>
